7e6fef92f1e3a95bc28ec02e47b71446c34a1e36
Jahanara Khatun <zarakhatun58@gmail.com>
2025-08-11 20:17:11 +0530

    update config

---
+++ src/app.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// src/app.ts
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
+const assessmentRoutes_1 = __importDefault(require("./routes/assessmentRoutes"));
+const adminQuestionRoutes_1 = __importDefault(require("./routes/adminQuestionRoutes"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+// app.use(cors());
+app.use((0, cors_1.default)({
+    origin: 'https://test-exam-client.onrender.com',
+    credentials: true
+}));
+app.use(express_1.default.json());
+app.use('/api/auth', authRoutes_1.default);
+app.use('/api/assessment', assessmentRoutes_1.default);
+app.use('/api/admin', adminQuestionRoutes_1.default);
+exports.default = app;
+//# sourceMappingURL=app.js.map
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -10,7 +10,7 @@ router.post('/verify-otp', verifyOTP);
 router.post('/login', login);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', resetPassword);
-router.get('/profile', verifyToken, async (req: any, res) => {
+router.get('/profile', verifyToken, async (req: any, res:any) => {
   try {
     const user = await SchoolUser.findById(req.user.id).select('-password -otp -otpExpires -passwordResetToken -passwordResetExpires');
     if (!user) return res.status(404).json({ success: false, message: 'User not found' });
